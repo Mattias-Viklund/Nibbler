9397b7dc76f70a27ce7edd36d28b60398c430d31
Mattias Wiklund <Sewq90@users.noreply.github.com>
2018-09-25 15:52:27 +0200

    pusz

---
+++ Nibbler/Nibbler/Nibble.cs
@@ -0,0 +1,62 @@
+﻿namespace Nibbler
+{
+    class Nibble
+    {
+        public const byte MaxValue = 16;
+        public const byte MinValue = 0;
+
+        public byte MergeByteNibble(byte byt, byte nibble, bool first, bool nibbleFirst)
+        {
+            byte nibb = (byte)(nibble & 0b00001111);
+
+            if (nibbleFirst)
+            {
+                byte b = byt;
+
+                if (first)
+                    b = (byte)((byt & 0b11110000) >> 4);
+                else
+                    b = (byte)(byt & 0b00001111);
+
+                if (nibbleFirst)
+                    b = (byte)(b | (nibble << 4));
+                else
+                    b = (byte)((b << 4) | nibble);
+
+                return b;
+
+            }
+            else
+            {
+                byte b = byt;
+
+                if (first)
+                    b = (byte)((byt & 0b11110000) >> 4);
+                else
+                    b = (byte)(byt & 0b00001111);
+
+                if (nibbleFirst)
+                    b = (byte)(b | (nibble << 4));
+                else
+                    b = (byte)((b << 4) | nibble);
+
+                return b;
+
+            }
+        }
+
+        public byte ByteToNibble(byte b, bool first)
+        {
+            if (first)
+            {
+                return (byte)(b >> 4);
+
+            }
+            else
+            {
+                return (byte)(b & 0b00001111);
+
+            }
+        }
+    }
+}
--- Nibbler/Nibbler/Program.cs
+++ Nibbler/Nibbler/Program.cs
@@ -3,20 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
 
 namespace Nibbler
 {
     class Program
     {
         static Memory mem = new Memory();
+        static bool running = true;
 
         static void Main(string[] args)
         {
-            for (byte i = 0; i < 16; i++)
+            for (byte b = 0; b < Nibble.MaxValue; b++)
             {
-                Console.WriteLine((int)mem.GetNibble(i));
+                Console.WriteLine((int)mem.GetNibble(b));
 
             }
+
+            for (byte b = 0; b < Nibble.MaxValue / 2; b++)
+            {
+                Console.WriteLine(b+": "+(int)mem.GetNibblePair(b));
+
+            }
+
+
+            mem.SetNibble(0b0111_1111);
 
             Console.ReadLine();
 
@@ -40,9 +52,9 @@ namespace Nibbler
 
         public byte GetNibble(byte nibble, bool withPosition = false)
         {
-            int nibb = nibble & 0b00001111;
+            byte nibb = (byte)(nibble & 0b00001111);
 
-            if (nibb % 2 == 0)
+            if (IsEven(nibb))
             {
                 if (withPosition)
                     return (byte)((nibbles[nibb / 2] >> 4) | (nibb << 4));
@@ -59,15 +71,48 @@ namespace Nibbler
             }
         }
 
+        private bool IsEven(byte b)
+        {
+            if ((b & 0b0000_0001) == 0b0000_0001)
+                return false;
+
+            return true;
+
+        }
+
         /// <summary>
-        /// 
+        /// 0b POSITION DATA (0000 0000)
         /// </summary>
         /// <param name="nibble"></param>
         /// <returns></returns>
-        public byte SetNibble(byte nibble)
+        public void SetNibble(byte nibble)
         {
-            byte data = GetNibble((byte)(nibble & 0b11110000));
+            byte pos = (byte)(nibble & 0b0111_0000);
+            bool second = false;
 
+            if (((byte)(nibble & 0b1000_0000) == 0b1000_0000))
+                second = true;
+
+            byte data = GetNibble((byte)(pos >> 4));
+
+
+            byte nibb = (byte)(nibble & 0b00001111);
+
+            if (nibb % 2 == 0)
+                nibbles[nibb / 2] = data;
+            else
+                nibbles[nibb / 2] = data;
+
+        }
+
+        public byte GetNibblePair(byte nibble)
+        {
+            byte nibb = (byte)(nibble & 0b00001111);
+            byte nibb3 = (byte)(nibb / 2);
+
+            if (!IsEven(nibb))
+                nibb3 = (byte)((nibb / 2) + 1);
+            return nibbles[nibb3];
 
         }
     }
